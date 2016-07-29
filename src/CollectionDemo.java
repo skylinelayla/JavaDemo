ac64d212bfc58411a90df98561aa25ec3739999b
SHIKUN <skylinelayla2012@gmail.com>
2016-07-29 10:35:11 +0800

    add collection method

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -10,10 +10,22 @@ public class CollectionDemo {
         al.add("java1");
         al.add("java2");
         al.add("java3");
-        al.add("java4");
+        al.add("java4");//集合中存储都是对象的引用，即地址
 
+        ArrayList al2 = new ArrayList();
+        al2.add("java1");
+        al2.add("java2");
+        al2.add("java5");
+        al2.add("java3");
+
+        System.out.println(al.retainAll(al2));//仅保留了交集，即al中和al2相同的元素。
+        System.out.println(al.removeAll(al2));//去掉al中al2相同的元素。
+
+        System.out.println(al);
         System.out.println(al.size());
-
-
+        al.remove(1);
+        System.out.println(al);
+        System.out.println(al.contains("java3"));
+        System.out.println(al.isEmpty());
     }
 }
