53c308b1a3f53ae8533f1f6b0cd3b8abc30610c5
SHIKUN <skylinelayla2012@gmail.com>
2016-07-29 11:50:18 +0800

    iterator

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by SHIKUN on 2016/7/29.
@@ -18,6 +19,18 @@ public class CollectionDemo {
         al2.add("java5");
         al2.add("java3");
 
+        Iterator it= al2.iterator();//返回一个Iterator接口,获取迭代器，去除元素，何为迭代器，就是取出元素的方式
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.hasNext());
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        for(Iterator ii=al2.iterator();ii.hasNext();) {
+            System.out.println(ii.next());
+        }
         System.out.println(al.retainAll(al2));//仅保留了交集，即al中和al2相同的元素。
         System.out.println(al.removeAll(al2));//去掉al中al2相同的元素。
 
