9dac26fb0ebe9e535e9d9d4d62eedfa7d8f85b78
SHIKUN <skylinelayla2012@gmail.com>
2016-08-13 16:42:46 +0800

    add new file

---
+++ src/test/SetDemoTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* SetDemo Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>���� 9, 2016</pre> 
+* @version 1.0 
+*/ 
+public class SetDemoTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
